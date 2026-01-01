221c69c4e7aae9e477240f74896157135e0978f8
Atharv Sankpal <atharvsankpal69420@gmail.com>
2026-01-01 16:34:41 +0530

    feat: setup db with drizzle

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,13 @@
 import { config } from "dotenv"
+import path from "path"
+config({ path: path.join(__dirname, `../../env.${process.env.ENVIRONMENT ?? ""}`) })
 
-config()
-
-const { PORT, NODE_ENV } = process.env
+const { PORT, NODE_ENV, DB_URI } = process.env
 const Config = {
-  PORT,
+  PORT: PORT ?? 8081,
   isDev: NODE_ENV === "development",
-}
+  DB_URI: DB_URI ?? "postgresql://root:root@localhost:5432/auth",
+} as const // make read only
 
 Object.freeze(Config)
 
---
+++ src/drizzle/drizzle.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/node-postgres"
+
+import Config from "../config"
+
+// You can specify any property from the node-postgres connection options
+export const db = drizzle({
+  connection: {
+    connectionString: Config.DB_URI,
+    ssl: true,
+  },
+})
