dc247c6ed5d772783bbd857c7aa6a29c15a6cf79
Atharv Sankpal <atharvsankpal69420@gmail.com>
2026-01-04 17:29:52 +0530

    feat: add index on email for users table

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,12 +1,16 @@
 import bcrypt from "bcrypt"
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
+import { integer, pgTable, uniqueIndex, varchar } from "drizzle-orm/pg-core"
 
-export const users = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  fullName: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255 }).notNull(),
-})
+export const users = pgTable(
+  "users",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    fullName: varchar({ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    password: varchar({ length: 255 }).notNull(),
+  },
+  (table) => [uniqueIndex("users_email_idx").on(table.email)],
+)
 export type IUser = typeof users.$inferSelect
 
 export const hashPassword = async (password: string): Promise<string> => {
